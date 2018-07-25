5968f1aafa8b9e5c9b82e188814a13497a026cc7
Catalin Coconeanu <catalincoconeanu@gmail.com>
2018-07-25 22:39:59 +0200

    O365&Azure

---
+++ O365 - View all the Distribution Groups a user is a member of with PowerShell in Office 365.ps1
@@ -0,0 +1,4 @@
+﻿#Find all the Distribution Groups a user is a member of with PowerShell in Office 365
+$Username = "hmorrill@leprinofoods.com"
+$DistributionGroups= Get-DistributionGroup -ResultSize Unlimited| where { (Get-DistributionGroupMember $_.Name | foreach {$_.PrimarySmtpAddress}) -contains "$Username"}
+$DistributionGroups
---
+++ O365 - View all the Distribution Groups a user is a member off - WORKS BETTER.ps1
@@ -0,0 +1,7 @@
+﻿#Find all the Distribution Groups a user is a member of with PowerShell in Office 365 - THIS WORKS FASTER AND NO ERRORS
+Write-Host "This tool displays the Distrbution lists a user is a member of"
+$User = Read-Host -Prompt 'email address you would like to find the distribution lists of'
+$Mailbox=get-Mailbox "$User"
+$DN=$mailbox.DistinguishedName
+$Filter = "Members -like ""$DN"""
+Get-DistributionGroup -ResultSize Unlimited -Filter $Filter
