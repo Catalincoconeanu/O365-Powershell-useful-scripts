c4a8950587b7258eb02149304b696191f2f2f9d8
Catalin Coconeanu <catalincoconeanu@gmail.com>
2018-07-28 22:03:22 +0200

    Update O365 - Add calendar permissions over X user calendar with sharing permissions flag delegate.ps1

--- O365 - Add calendar permissions over X user calendar with sharing permissions flag delegate.ps1
+++ O365 - Add calendar permissions over X user calendar with sharing permissions flag delegate.ps1
@@ -1,1 +1,2 @@
-﻿Add-MailboxFolderPermission -Identity user1@domain.com:\Calendar -User user2@domain.com  -AccessRights Editor -SharingPermissionFlags Delegate
+#-SharingPermissionFlags,CanViewPrivateItems
+Add-MailboxFolderPermission -Identity user1@domain.com:\Calendar -User user2@domain.com  -AccessRights Editor -SharingPermissionFlags Delegate
