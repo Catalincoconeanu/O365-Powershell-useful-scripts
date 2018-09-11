3ad2321c5301d07738574b4047638775d621a41c
Catalin Coconeanu <catalincoconeanu@gmail.com>
2018-09-11 17:06:14 +0200

    Update O365 - Check Skype Conference ID nr for X user, also how to set a new Conference ID.ps1

--- O365 - Check Skype Conference ID nr for X user, also how to set a new Conference ID.ps1
+++ O365 - Check Skype Conference ID nr for X user, also how to set a new Conference ID.ps1
@@ -1,3 +1,3 @@
 ﻿#Check Skype conference ID nr for X user
 #To set a new Conference ID: Set-CsOnlineDialInConferencingUser -Identity "Amos Marble"  -ResetConferenceID 827196 
-Get-CsOnlineDialInConferencingUser -Identity mteller@leprinofoods.com
+Get-CsOnlineDialInConferencingUser -Identity user@domain.com
