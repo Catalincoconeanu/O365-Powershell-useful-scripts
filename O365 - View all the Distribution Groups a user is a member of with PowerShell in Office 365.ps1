a32f41be8c733002ecbc735c168c81e150539035
Catalin Coconeanu <catalincoconeanu@gmail.com>
2018-09-23 15:31:45 +0200

    Update O365 - View all the Distribution Groups a user is a member of with PowerShell in Office 365.ps1

--- O365 - View all the Distribution Groups a user is a member of with PowerShell in Office 365.ps1
+++ O365 - View all the Distribution Groups a user is a member of with PowerShell in Office 365.ps1
@@ -1,4 +1,4 @@
-﻿#Find all the Distribution Groups a user is a member of with PowerShell in Office 365
+﻿#Find all the Distribution Groups a user is a member of with PowerShell in Office 365 - This script is not working 100%, depends of the environment 
 $Username = "user@domain.com"
 $DistributionGroups= Get-DistributionGroup -ResultSize Unlimited| where { (Get-DistributionGroupMember $_.Name | foreach {$_.PrimarySmtpAddress}) -contains "$Username"}
 $DistributionGroups
