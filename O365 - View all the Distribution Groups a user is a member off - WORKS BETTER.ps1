60c9ec5f82c9c929aa743846e9b36a302814712f
Catalin Coconeanu <catalincoconeanu@gmail.com>
2018-09-23 15:32:21 +0200

    Update O365 - View all the Distribution Groups a user is a member off - WORKS BETTER.ps1

--- O365 - View all the Distribution Groups a user is a member off - WORKS BETTER.ps1
+++ O365 - View all the Distribution Groups a user is a member off - WORKS BETTER.ps1
@@ -1,4 +1,4 @@
-﻿#Find all the Distribution Groups a user is a member of with PowerShell in Office 365 - THIS WORKS FASTER AND NO ERRORS
+﻿#Find all the Distribution Groups a user is a member of with PowerShell in Office 365 - THIS WORKS FASTER AND WITH NO ERRORS
 Write-Host "This tool displays the Distrbution lists a user is a member of"
 $User = Read-Host -Prompt 'email address you would like to find the distribution lists of'
 $Mailbox=get-Mailbox "$User"
