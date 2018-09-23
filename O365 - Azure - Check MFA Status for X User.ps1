684ad26660477dd2729edf82276c209b4af9ba25
Catalin Coconeanu <catalincoconeanu@gmail.com>
2018-09-23 14:10:49 +0200

    Rename O365 - Azure - Check MFA Status for X User.ps1 to O365- Check MFA Status for X User.ps1

--- O365 - Azure - Check MFA Status for X User.ps1
+++
@@ -1,2 +0,0 @@
-﻿#O365 - Azure - Check MFA status for X user
-Get-MsolUser -Userprincipalname user@domain.com | select DisplayName,UserPrincipalName,@{N="MFA Status"; E={ if( $_.StrongAuthenticationRequirements.State -ne $null){ $_.StrongAuthenticationRequirements.State} else { "Disabled"}}}
