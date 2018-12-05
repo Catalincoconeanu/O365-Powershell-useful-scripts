8619a9846a0ae57d2dcc6a25ae32cac790f5fca6
Catalin Coconeanu <catalincoconeanu@gmail.com>
2018-12-05 11:34:31 +0100

    O365

---
+++ O365-Check_last_sync_time.ps1
@@ -0,0 +1,5 @@
+﻿#This will allow you to check last sync time for X user
+#You must be connect to O365 Service before use
+#Use Connect-MsolService
+$User = Read-Host -Prompt 'Enter Email Address'
+get-msoluser -UserPrincipalName $User | Select-Object LastDirSyncTime, DirSyncProvisioningErrors, ValiadtionStatus
